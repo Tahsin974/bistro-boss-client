5f4973e9aa3a6d1ae1722b61d5b5264bb6579bae
Tahsin974 <tahsinislam974@gmail.com>
2024-11-24 12:54:53 +0600

    Chief Service Section Done

--- src/Pages/HomePage/CheifService/CheifService.jsx
+++ src/Pages/HomePage/CheifService/CheifService.jsx
@@ -1,10 +1,28 @@
-
+import img from '../../../assets/home/chef-service.jpg'
+import './CheifService.css'
 const CheifService = () => {
-    return (
-        <div>
+  return (
+    <div className="my-10">
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            `url(${img})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-5xl  lg:p-28 md:p-28 p-6  bg-white text-black   space-y-5">
+            <h1 className=" lg:text-5xl text-2xl font-bold rye-regular">Bistro Boss</h1>
+            <p>
+             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum quasi eligendi minima, officiis consequuntur aspernatur dolorem suscipit voluptate sequi dicta quaerat culpa, id vel dolores nam totam. Aliquam quasi odio possimus dolore. Neque architecto numquam ducimus necessitatibus eius reiciendis sunt ipsum, alias consectetur dolores illo iure culpa consequatur, perferendis sit!
+            </p>
             
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CheifService;
--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,12 +1,13 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import CheifService from "../CheifService/CheifService";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
-      
+      <CheifService></CheifService>
     </div>
   );
 };
